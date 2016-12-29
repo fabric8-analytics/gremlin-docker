7a9720e173be5e73eb190df8ff63999321bcfaf3
Shubham Minglani <shubham@linux.com>
2016-12-30 02:13:46 +0530

    enable client to talk to remote gremlin using env

--- gremlin-client/scripts/entrypoint-client.sh
+++ gremlin-client/scripts/entrypoint-client.sh
@@ -8,9 +8,22 @@ export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
+GREMLIN_DEFAULT_HOST="gremlin"
+
 if [ $REST -eq 1 ] || [ $PYTHON -eq 1 ]; then
     ipython
 else
-    cd ${SERVER_DIR}
-    exec bin/gremlin.sh 
+
+    if [ -v GREMLIN_HOST ]; then
+				sed -i.bckp 's#hosts: .*#hosts: ['$GREMLIN_HOST']#' "${SERVER_DIR}/conf/remote.yaml"
+		else
+				sed -i.bckp 's#hosts: .*#hosts: ['$GREMLIN_DEFAULT_HOST']#' "${SERVER_DIR}/conf/remote.yaml"
+		fi
+
+    if [ -v GREMLIN_PORT ]; then
+	      sed -i.bckp 's#port: .*#port: '$GREMLIN_PORT'#' "${SERVER_DIR}/conf/remote.yaml"
+	  fi
+
+    cd ${SERVER_DIR} &&\
+    (echo ":remote connect tinkerpop.server conf/remote.yaml" && cat) | bin/gremlin.sh
 fi
