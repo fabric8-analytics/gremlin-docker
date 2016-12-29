2230752962df652fe618bc496877a0d734d2285f
Shubham Minglani <shubham@linux.com>
2016-12-29 21:02:06 +0530

    add support for random user in openshift

--- gremlin-client/scripts/entrypoint-client.sh
+++ gremlin-client/scripts/entrypoint-client.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/bash
 
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+export GREMLIN_HOME="/opt/dynamodb/gremlin_home/"
+envsubst < /tmp/passwd.template > /tmp/passwd
+export LD_PRELOAD=libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
 if [ $REST -eq 1 ] || [ $PYTHON -eq 1 ]; then
     ipython
 else
