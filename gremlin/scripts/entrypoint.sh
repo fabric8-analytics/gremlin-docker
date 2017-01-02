eb224ad1332579097e00c55dcf0a19e1be44de88
Shubham Minglani <shubham@linux.com>
2017-01-02 18:27:49 +0530

    make storage.dynamodb.client.endpoint more flexible

--- gremlin/scripts/entrypoint.sh
+++ gremlin/scripts/entrypoint.sh
@@ -9,6 +9,10 @@ sed -i.bckp 's#host: .*#host: '$GREMLIN_HOST'#' ${GREMLIN_CONF}
 sed -i.bckp 's#storage.dynamodb.client.credentials.class-name=.*#storage.dynamodb.client.credentials.class-name='$1'#' ${PROPS}
 sed -i.bckp 's#storage.dynamodb.client.credentials.constructor-args=.*#storage.dynamodb.client.credentials.constructor-args='$2'#' ${PROPS}
 
+if [ -v aws_region ]; then
+  sed -i.bckp 's#storage.dynamodb.client.endpoint=.*#storage.dynamodb.client.endpoint='$aws_region'#' ${PROPS}
+fi
+
 if [ "$REST" == "1" ]; then
     sed -i.bckp 's#channelizer: .*#channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer#' ${GREMLIN_CONF}
 fi
