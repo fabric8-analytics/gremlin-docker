e347741cc95c851f642062c75b2761067942e5a1
yzainee <37098367+yzainee@users.noreply.github.com>
2018-06-13 12:34:31 +0530

    Janus 1.1.0 (#43)
    * #2157 migrate from titan to janus

--- scripts/entrypoint-local.sh
+++ scripts/entrypoint-local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-SERVER_DIR=dynamodb-titan-storage-backend/server/dynamodb-titan100-storage-backend-1.0.0-hadoop1/
+SERVER_DIR=dynamodb-janusgraph-storage-backend/server/dynamodb-janusgraph-storage-backend-1.2.0/
 PROPS=${SERVER_DIR}/conf/gremlin-server/dynamodb-local.properties
 GREMLIN_CONF=${SERVER_DIR}/conf/gremlin-server/gremlin-server-local.yaml
 DYNAMO_HOST=${DYNAMO_HOST:-dynamodb}
@@ -9,6 +9,9 @@ GREMLIN_HOST=0.0.0.0
 SCRIPT_TIMEOUT=100000
 RESPONSE_TIMEOUT=100000
 export JAVA_OPTIONS="-Xms256m -Xmx8192m -javaagent:/opt/dynamodb/$SERVER_DIR/lib/jamm-0.3.0.jar"
+STORAGE_BACKEND=com.amazon.janusgraph.diskstorage.dynamodb.DynamoDBStoreManager
+USE_TITAN_IDS=true
+TITAN_IDS=titan_ids
 
 echo "DYNAMO_HOST is set to ${DYNAMO_HOST}, DYNAMO_PORT is set to ${DYNAMO_PORT}"
 
@@ -17,6 +20,12 @@ sed -i.bckp 's#scriptEvaluationTimeout: .*#scriptEvaluationTimeout: '${SCRIPT_TI
 sed -i.bckp 's#serializedResponseTimeout: .*#serializedResponseTimeout: '${RESPONSE_TIMEOUT}'#' ${GREMLIN_CONF}
 
 sed -i.bckp 's#storage.dynamodb.client.endpoint=.*#storage.dynamodb.client.endpoint=http://'$DYNAMO_HOST':'$DYNAMO_PORT'#' ${PROPS}
+
+sed -i.bckp 's#storage.backend=.*#storage.backend='${STORAGE_BACKEND}'#' ${PROPS}
+
+sed -i.bckp 's#storage.dynamodb.use-titan-ids=.*#storage.dynamodb.use-titan-ids='${USE_TITAN_IDS}'#' ${PROPS}
+
+sed -i.bckp 's#storage.dynamodb.stores.ids.store-name=.*#storage.dynamodb.stores.ids.store-name='${TITAN_IDS}'#' ${PROPS}
 
 echo "Setup code metrics configuration"
 
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,14 @@
 #! /usr/bin/bash
 
 # Files for which configuration needs to change
-SERVER_DIR=dynamodb-titan-storage-backend/server/dynamodb-titan100-storage-backend-1.0.0-hadoop1/
+SERVER_DIR=dynamodb-janusgraph-storage-backend/server/dynamodb-janusgraph-storage-backend-1.1.0/
 PROPS=${SERVER_DIR}/conf/gremlin-server/dynamodb.properties
 GREMLIN_CONF=${SERVER_DIR}/conf/gremlin-server/gremlin-server.yaml
 GREMLIN_HOST=0.0.0.0
 UUID=$(cat /proc/sys/kernel/random/uuid)
+STORAGE_BACKEND=com.amazon.janusgraph.diskstorage.dynamodb.DynamoDBStoreManager
+USE_TITAN=true
+USE_OLD_TITAN=titan_ids
 
 export JAVA_OPTIONS=${JAVA_OPTIONS:- -Xms512m -Xmx2048m}
 
@@ -20,6 +23,15 @@ if grep -i '^graph.unique-instance-id=' "$PROPS" 1>/dev/null; then
 else
     echo "graph.unique-instance-id=${UUID}" >> ${PROPS}
 fi
+
+echo "graph.titan-version=1.1.0-SNAPSHOT" >> ${PROPS}
+
+sed -i.bckp 's#consoleReporter: .*#consoleReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#csvReporter: .*#csvReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#jmxReporter: .*#jmxReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#slf4jReporter: .*#slf4jReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#gangliaReporter: .*#gangliaReporter: {enabled: false}, #' ${GREMLIN_CONF}
+sed -i.bckp 's#graphiteReporter: .*#graphiteReporter: {enabled: false}, #' ${GREMLIN_CONF}
 
 if [ -n "$DYNAMODB_CLIENT_CREDENTIALS_CLASS_NAME" ]; then
     sed -i.bckp 's#storage.dynamodb.client.credentials.class-name=.*#storage.dynamodb.client.credentials.class-name='${DYNAMODB_CLIENT_CREDENTIALS_CLASS_NAME}'#' ${PROPS}
@@ -58,20 +70,51 @@ if [ -n "$DYNAMODB_PREFIX" ]; then
 fi
 
 if [ -n "$WRITE_UNITS" ]; then
-    sed -i.bckp 's#storage.dynamodb.stores.edgestore.capacity-write=.*#storage.dynamodb.stores.edgestore.capacity-write='$WRITE_UNITS'#' ${PROPS}
-    sed -i.bckp 's#storage.dynamodb.stores.graphindex.capacity-write=.*#storage.dynamodb.stores.graphindex.capacity-write='$WRITE_UNITS'#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.edgestore.initial-capacity-write=.*#storage.dynamodb.stores.edgestore.initial-capacity-write='$WRITE_UNITS'#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.graphindex.initial-capacity-write=.*#storage.dynamodb.stores.graphindex.initial-capacity-write='$WRITE_UNITS'#' ${PROPS}
 else
-    sed -i.bckp 's#storage.dynamodb.stores.edgestore.capacity-write=.*#storage.dynamodb.stores.edgestore.capacity-write=25#' ${PROPS}
-    sed -i.bckp 's#storage.dynamodb.stores.graphindex.capacity-write=.*#storage.dynamodb.stores.graphindex.capacity-write=25#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.edgestore.initial-capacity-write=.*#storage.dynamodb.stores.edgestore.initial-capacity-write=25#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.graphindex.initial-capacity-write=.*#storage.dynamodb.stores.graphindex.initial-capacity-write=25#' ${PROPS}
 fi
 
 if [ -n "$READ_UNITS" ]; then
-    sed -i.bckp 's#storage.dynamodb.stores.edgestore.capacity-read=.*#storage.dynamodb.stores.edgestore.capacity-read='$READ_UNITS'#' ${PROPS}
-    sed -i.bckp 's#storage.dynamodb.stores.graphindex.capacity-read=.*#storage.dynamodb.stores.graphindex.capacity-read='$READ_UNITS'#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.edgestore.initial-capacity-read=.*#storage.dynamodb.stores.edgestore.initial-capacity-read='$READ_UNITS'#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.graphindex.initial-capacity-read=.*#storage.dynamodb.stores.graphindex.initial-capacity-read='$READ_UNITS'#' ${PROPS}
 else
-    sed -i.bckp 's#storage.dynamodb.stores.edgestore.capacity-read=.*#storage.dynamodb.stores.edgestore.capacity-read=25#' ${PROPS}
-    sed -i.bckp 's#storage.dynamodb.stores.graphindex.capacity-read=.*#storage.dynamodb.stores.graphindex.capacity-read=25#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.edgestore.initial-capacity-read=.*#storage.dynamodb.stores.edgestore.initial-capacity-read=25#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.stores.graphindex.initial-capacity-read=.*#storage.dynamodb.stores.graphindex.initial-capacity-read=25#' ${PROPS}
 fi
+
+if grep -i '^storage.backend=' "$PROPS" 1>/dev/null; then
+    sed -i.bckp 's#storage.backend=.*#storage.backend='${STORAGE_BACKEND}'#' ${PROPS}
+ else
+    echo "storage.backend=$STORAGE_BACKEND" >> ${PROPS}
+fi
+
+if grep -i '^storage.dynamodb.prefix=' "$PROPS" 1>/dev/null; then
+    sed -i.bckp 's#storage.dynamodb.prefix=.*#storage.dynamodb.prefix='${DYNAMODB_PREFIX}'#' ${PROPS}
+ else
+    echo "storage.dynamodb.prefix=$DYNAMODB_PREFIX" >> ${PROPS}
+fi
+
+if grep -i '^storage.dynamodb.client.signing-region=' "$PROPS" 1>/dev/null; then
+    sed -i.bckp 's#storage.dynamodb.client.signing-region=.*#storage.dynamodb.client.signing-region='${AWS_DEFAULT_REGION}'#' ${PROPS}
+ else
+    echo "storage.dynamodb.client.signing-region=$AWS_DEFAULT_REGION" >> ${PROPS}
+fi
+
+if grep -i '^storage.dynamodb.use-titan-ids=' "$PROPS" 1>/dev/null; then
+    sed -i.bckp 's#storage.dynamodb.use-titan-ids=.*#storage.dynamodb.use-titan-ids='${USE_TITAN}'#' ${PROPS}
+ else
+    echo "storage.dynamodb.use-titan-ids=$USE_TITAN" >> ${PROPS}
+fi
+
+if grep -i '^ids.store-name=' "$PROPS" 1>/dev/null; then
+    sed -i.bckp 's#ids.store-name=.*#ids.store-name='${USE_OLD_TITAN}'#' ${PROPS}
+ else
+    echo "ids.store-name=$USE_OLD_TITAN" >> ${PROPS}
+fi
+
 
 if [ -n "${DATA_MODEL}" ] && ( [ "$DATA_MODEL" = "SINGLE" ] || [ "$DATA_MODEL" = "MULTI" ] ); then
     if grep -i '^storage.dynamodb.stores.edgestore.data-model=' "$PROPS" 1>/dev/null; then
