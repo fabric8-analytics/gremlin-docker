60a3e92d6daf394fac4ec88c9c2417915f84d1da
Yusuf Zainee <yzainee@redhat.com>
2018-06-18 12:19:30 +0530

    changed config names

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,8 +7,8 @@ GREMLIN_CONF=${SERVER_DIR}/conf/gremlin-server/gremlin-server.yaml
 GREMLIN_HOST=0.0.0.0
 UUID=$(cat /proc/sys/kernel/random/uuid)
 STORAGE_BACKEND=com.amazon.janusgraph.diskstorage.dynamodb.DynamoDBStoreManager
-USE_TITAN=true
-USE_OLD_TITAN=titan_ids
+USE_TITAN_IDS=true
+TITAN_IDS=titan_ids
 
 export JAVA_OPTIONS=${JAVA_OPTIONS:- -Xms512m -Xmx2048m}
 
@@ -104,15 +104,15 @@ if grep -i '^storage.dynamodb.client.signing-region=' "$PROPS" 1>/dev/null; then
 fi
 
 if grep -i '^storage.dynamodb.use-titan-ids=' "$PROPS" 1>/dev/null; then
-    sed -i.bckp 's#storage.dynamodb.use-titan-ids=.*#storage.dynamodb.use-titan-ids='${USE_TITAN}'#' ${PROPS}
+    sed -i.bckp 's#storage.dynamodb.use-titan-ids=.*#storage.dynamodb.use-titan-ids='${USE_TITAN_IDS}'#' ${PROPS}
  else
-    echo "storage.dynamodb.use-titan-ids=$USE_TITAN" >> ${PROPS}
+    echo "storage.dynamodb.use-titan-ids=USE_TITAN_IDS" >> ${PROPS}
 fi
 
 if grep -i '^ids.store-name=' "$PROPS" 1>/dev/null; then
-    sed -i.bckp 's#ids.store-name=.*#ids.store-name='${USE_OLD_TITAN}'#' ${PROPS}
+    sed -i.bckp 's#ids.store-name=.*#ids.store-name='${TITAN_IDS}'#' ${PROPS}
  else
-    echo "ids.store-name=$USE_OLD_TITAN" >> ${PROPS}
+    echo "ids.store-name=TITAN_IDS" >> ${PROPS}
 fi
 
 
